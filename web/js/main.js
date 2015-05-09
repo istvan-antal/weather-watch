22abb252ee2d91b8fd8792682c7c041341ee3bda
Antal István Miklós <istvan.m.antal@gmail.com>
2015-05-09 07:39:50 +0100

    Use modules implemented in TypeScript int he JS code

--- web/js/main.js
+++ web/js/main.js
@@ -13,10 +13,9 @@ $(function () {
         currentUnit = 'fahrenheit',
         currentData,
         mapOptions,
-        map,        
-        iconMap;
+        map;
 
-    mapOptions = {   
+    mapOptions = {
         center: {
             lat: -34.397,
             lng: 150.644
@@ -29,77 +28,55 @@ $(function () {
         overviewMapControl: false,
         zoom: 8
     };
-    
+
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-    
-    iconMap = {
-        'partly-cloudy-night': 'night-partly-cloudy',
-        'clear-day': 'day-sunny',
-        'clear-night': 'night-clear',
-        'rain': 'rain',
-        'snow': 'snow',
-        'sleet': 'sleet',
-        'wind': 'day-windy',
-        'fog': 'fog',
-        'cloudy': 'cloudy',
-        'partly-cloudy-day': 'night-cloudy',
-        'hail': 'hail',
-        'thunderstorm': 'thunderstorm',
-        'tornado': 'tornado',
-        'default': 'alien'
-    };
 
     searchForm.submit(function () {
-        $.ajax({
-            url: '/api/weather',
-            data: {
-                address: searchField.val()
-            }
-        }).then(function (data) {
+        api.fetchCurrentWeather(searchField.val()).then(function (data) {
             currentData = data;
             renderCurrentData();
         });
 
         return false;
     });
-    
+
     unitSelector.on('click', 'button', function () {
         var element = $(this);
-        
+
         currentUnit = element.data('value');
         unitSelectorButtons.removeClass('active');
         element.addClass('active');
-        
+
         renderCurrentData();
     });
-    
+
     function renderCurrentData() {
         if (!currentData) {
             return;
         }
-        
+
         tempature.html(
             Math.round(currentData.temperature[currentUnit]) +
             '<i class="wi wi-' + currentUnit + '"/>'
         );
         summary.text(currentData.summary);
-        
+
         var comparismText  = '',
             temperatureDiff;
-    
+
         temperatureDiff = currentData.temperature.celsius -
                           currentData.yesterday.temperature.celsius;
-        
+
         if (temperatureDiff < 0) {
             comparismText = 'colder than yesterday';
         }
-        
+
         if (temperatureDiff > 0) {
             comparismText = 'warmer than yesterday';
         }
-        
+
         comparism.text(comparismText);
-        
+
         icon.attr('class', getWeatherIconClass(currentData.icon));
 
         map.setCenter({
@@ -109,15 +86,15 @@ $(function () {
 
         results.removeClass('invisible');
     }
-    
+
     function getWeatherIconClass(icon) {
         var mappedIcon = iconMap[icon];
-        
+
         if (!mappedIcon) {
             mappedIcon = iconMap['default'];
         }
-        
+
         return 'pull-right weather-icon wi wi-' + iconMap[icon];
     }
-    
+
 });
